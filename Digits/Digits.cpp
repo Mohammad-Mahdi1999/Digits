7429e7f9e9f72bc9a44ed484733a5f5c37cc145f
Mohammad-Mahdi1999 <44753085+Mohammad-Mahdi1999@users.noreply.github.com>
2018-11-27 12:59:50 +0330

    Using Array

--- Digits/Digits.cpp
+++ Digits/Digits.cpp
@@ -1,19 +1,27 @@
 // In the name of God !
 //Play with figures !
-//v1.0
+//v1.7
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
 
 void main()
 {
-	int num, sum = 0;
+	int num, figure[100], i=0, j=0 ,sum = 0;
 	cout << "Enter a number :";
 	cin >> num;
+	if (num < 0)
+		num = num*-1;
 	while (num>0)
 	{
-		sum = sum + num % 10;
+		figure[i] = num%10;
 		num = num / 10;
+		i++;
+	}
+	while (j<i)
+	{
+		sum = figure[j] + sum;
+		j++;
 	}
 	cout << "The sum of figures is " << sum << " !" << endl;
 	cin.get();
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Digits
 Playing with digits
 Sum of the Digits!
+
+v1.7 : Using Array 
